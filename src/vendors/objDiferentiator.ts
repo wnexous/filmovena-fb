1d66e90801fb292c0bcd80070c9b4f7de9f046c1
wnexous <andreneexus@hotmail.com>
2024-10-25 19:05:55 -0300

    fix: eslint error

--- src/vendors/objDiferentiator.ts
+++ src/vendors/objDiferentiator.ts
@@ -9,7 +9,10 @@ export default function objDiferentiator(matriz: object, diference: object) {
             const diferenceData = diference[key as keyof object]
 
             if (matrizData != diferenceData) objResult[key as keyof object] = diferenceData
-        } catch (err) { }
+        } catch (err) { 
+            console.log('err', err)
+
+        }
 
 
     })
