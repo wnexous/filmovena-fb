e47dc04e089cd2819a3dd9a82e5053d8a1b4c428
wnexous <andreneexus@hotmail.com>
2024-11-10 21:25:46 -0300

    fix: get inner sql

--- src/sql/index.ts
+++ src/sql/index.ts
@@ -1,8 +1,20 @@
-import { readFileSync } from "fs";
-import path from "path";
 
+
+/**
+ * Tive que colocar a query chumbada aqui pois nao estava conseguindo ler o diretorio dentro da vercel
+ */
 const sql = {
-    filmeListagem: readFileSync(path.join("src", "sql", "filmeListagem.sql"), "utf-8")
+    filmeListagem: `SELECT
+	f.Id, 
+    f.Nome, 
+    f.Tempo_duracao, 
+    f.Dt_Lanc, 
+    f.IMDB, 
+    fe.Descricao AS FaixaEtaria_Descricao,
+    p.Nome AS Produtora_Nome
+FROM Filme AS f
+  INNER JOIN FaixaEtaria AS fe ON f.fk_FaixaEtaria_Id = fe.Id
+  INNER JOIN Produtora AS p ON p.Id = f.fk_Produtora_Id;`
 }
 
 export default sql
